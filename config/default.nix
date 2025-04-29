31182b32b0f635ffe66698d9b26f85d60e70db10
Max Friebe <mfriebe@hachmeister-partner.de>
2025-04-29 14:12:43 +0200

    refactored plugins

--- config/default.nix
+++ config/default.nix
@@ -1,18 +1,7 @@
 {
   # Plugins
   imports = [ 
-  	./bufferline.nix 
-	./lualine.nix
-	./lsp.nix
-	./blink-cmp.nix
-	./telescope.nix
-	./oil.nix
-	./treesitter.nix
-	./luasnip.nix
-	./gitsigns.nix
-	./lazygit.nix
-	./noice.nix
-	./indent-blankline.nix
+  	./plugins
 	];
 
   # Color scheme
---
+++ config/plugins/default.nix
@@ -0,0 +1,17 @@
+{
+  # Plugins
+  imports = [ 
+  	./bufferline.nix 
+	./lualine.nix
+	./lsp.nix
+	./blink-cmp.nix
+	./telescope.nix
+	./oil.nix
+	./treesitter.nix
+	./luasnip.nix
+	./gitsigns.nix
+	./lazygit.nix
+	./noice.nix
+	./indent-blankline.nix
+	];
+}
