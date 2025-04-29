cc65c1f2b447f37ed581762a87d3523d767e069e
Max Friebe <mfriebe@hachmeister-partner.de>
2025-04-29 14:53:07 +0200

    nvim-autopairs

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -2,17 +2,18 @@
   # Plugins
   imports = [ 
   	./bufferline.nix 
-	./lualine.nix
-	./lsp.nix
-	./blink-cmp.nix
-	./telescope.nix
-	./oil.nix
-	./treesitter.nix
-	./luasnip.nix
-	./gitsigns.nix
-	./lazygit.nix
-	./noice.nix
-	./indent-blankline.nix
-	./chadtree.nix
-	];
+    ./lualine.nix
+    ./lsp.nix
+    ./blink-cmp.nix
+    ./telescope.nix
+    ./oil.nix
+    ./treesitter.nix
+    ./luasnip.nix
+    ./gitsigns.nix
+    ./lazygit.nix
+    ./noice.nix
+    ./indent-blankline.nix
+    ./chadtree.nix
+    ./nvim-autopairs.nix
+  ];
 }
